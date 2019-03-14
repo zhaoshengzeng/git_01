06331e06d458561ed67e28d42f95706540c89fb4
zhaoshengzeng <3166377623@qq.com>
2019-03-14 17:19:14 +0800

    dev 提交Student.java

---
+++ src/main/java/com/huayu/git/controller/bean/Student.java
@@ -0,0 +1,38 @@
+package com.huayu.git.controller.bean;
+
+/**
+ * @Author zhao
+ * @Date 2019/3/14 17:15
+ * @Description:
+ **/
+public class Student {
+    /**
+     * 账号id
+     */
+    private String numberid;
+
+    /**
+     * 学生id
+     */
+    private String sid;
+
+    /**
+     * 教师id
+     */
+    private String tid;
+
+    /**
+     * 账号
+     */
+    private String account;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 角色Id
+     */
+    private Integer rid;
+}
